ce5249fdab0d98e1c8ee75855d3f5ee605140069
srgray17 <55286195+srgray17@users.noreply.github.com>
2020-04-18 15:07:26 -0400

    Add files via upload

---
+++ Q3/main.js
@@ -0,0 +1,37 @@
+//https://jessicagilfillan.github.io/Final_Exam_Prep/Q3/cats.json
+
+let button = document.querySelector("button");
+
+let catsJson = fetch("https://jessicagilfillan.github.io/Final_Exam_Prep/Q3/cats.json ")
+.then(function(response) {
+  return response.json();
+});
+
+button.onclick = displayCats;
+
+  function displayCats(jsonObj) {
+    let cats = jsonObj.cats;
+
+    for (let i = 0; i < cats.length; i++) {
+
+      let article = document.createElement("article");
+      let h2 = document.createElement("h2");
+      let p1 = document.createElement("p");
+      let p2 = document.createElement("p");
+      let p3 = document.createElement("p");
+
+      let section = document.querySelector("section");
+
+      h2.textContent = cats[i].name;
+      p1.textContent = "Species: " + cats[i].species;
+      p2.textContent = "Favorite Foods: " + cats[i].favFoods;
+      p3.textContent = "Age: " + cats[i].age;
+
+
+      article.appendChild(h2);
+      article.appendChild(p1);
+      article.appendChild(p2);
+      article.appendChild(p3);
+      section.appendChild(article);
+    }
+};
