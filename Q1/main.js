577a273ec448a2fe07c5abccd5275ac61e010691
srgray17 <55286195+srgray17@users.noreply.github.com>
2020-04-18 13:44:44 -0400

    Add files via upload

---
+++ Q1/main.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+let main = document.querySelector("main");
+let submitButton = document.getElementById("submit");
+let body = document.querySelector("body");
+
+submitButton.addEventListener("click", function(e) {
+  let personName = document.querySelector("input").value;
+  console.log(personName);
+  let para = document.createElement("p");
+  para.innerHTML = "Hey there " + personName + "! Thanks for stopping by!";
+  main.appendChild(para);
+  body.setAttribute("class", "meow");
+  console.log("=^..^=");
+});
